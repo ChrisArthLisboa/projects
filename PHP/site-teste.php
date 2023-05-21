8258ad14674f2a9fd7e09f9693e64ad0889952a1
Lisboa <131053411+ChrisArthLisboa@users.noreply.github.com>
2023-05-21 18:48:51 -0300

    Add files via upload

---
+++ PHP/site-teste.php
@@ -0,0 +1,25 @@
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Testando</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+	<h1>O lugar dos testes de verdade</h1>
+
+	<div id="input">
+		<input type="text" name="$Var" placeholder="Entrada">
+	</div>
+
+	<div id="php">
+		<?php 
+			include ('codes/testes.php');
+		?>
+	</div>
+</body>
+</html>
